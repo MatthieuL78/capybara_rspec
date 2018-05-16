934c2b333819b4110e56e3d76e11daa3de4b1849
MatthieuL78 <matthieu.lu@outlook.fr>
2018-05-16 15:23:43 +0200

    finish project

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,22 @@ class BooksController < ApplicationController
   def index
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    
+    book = Book.create(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
   end
 
 end
